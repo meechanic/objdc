f09a91fb9f8df1943797fe17de1d434ba7e14267
Mikhail Efremov <meechanic@gmail.com>
2022-08-14 22:59:16 +0300

    Add executable

--- objdc/__init__.py
+++ objdc/__init__.py
@@ -1,7 +1,7 @@
 '''
 The initialization module.
 '''
-from .common import (eprint, eprint_exception, sepval_load, sepval_dump, md_dump, SettingsContext,
+from .common import (parse_args, eprint, eprint_exception, sepval_load, sepval_dump, md_dump, SettingsContext,
     custom_open_input, custom_open_output, custom_close_input, custom_close_output,
     custom_load, custom_dump)
 
